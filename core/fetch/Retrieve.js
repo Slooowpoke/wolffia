fa28776b9f3e15ddc32a5555279bb72355cb0f98
Mali Coxon <malii.coxon@gmail.com>
2018-02-22 21:35:28 +0000

    Added getPage to Retrieve

--- core/fetch/Retrieve.js
+++ core/fetch/Retrieve.js
@@ -1,8 +1,13 @@
 import Connection from './Connection'
+import Page from '../models/Page'
 
 export default class Retrieve {
 
     constructor(){
         this.connection = new Connection()
+    }
+    async getPage(pagename){
+        let {blocks, meta} = await this.connection.queryForPage(pagename)
+        return new Page(meta, blocks)
     }
 }
