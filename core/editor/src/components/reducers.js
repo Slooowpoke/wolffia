6eb44d211b82b68654343a3c82c7517fbe7df3b2
Mali Coxon <malii.coxon@gmail.com>
2018-02-26 21:51:43 +0000

    Added blocks to reducers

--- core/editor/src/components/reducers.js
+++ core/editor/src/components/reducers.js
@@ -2,4 +2,5 @@ import {combineReducers} from 'redux'
 import {projects} from '../reducers/projects'
 import {authentication} from '../reducers/login'
 import {pages} from '../reducers/pages'
-export const app = combineReducers({projects, authentication, pages})
+import {blocks} from '../reducers/blocks'
+export const app = combineReducers({projects, authentication, pages, blocks})
