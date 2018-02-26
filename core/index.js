b298a61d0d48e19ad0295ef2850769b21acbc6d7
Mali Coxon <malii.coxon@gmail.com>
2018-02-26 22:00:04 +0000

    Added routes for basic pages

--- core/index.js
+++ core/index.js
@@ -11,6 +11,27 @@ const retriever = new Retrieve()
 app.use(views('./templates', {
 	extension: 'ejs'
 }))
+const pages = {
+	home: async(ctx) => {
+        ctx.state.page = await retriever.getPage('home')
+        await ctx.render(ctx.state.page.meta.template, ctx.state.page)
+	},
+    about: async(ctx) => {
+        // ctx.body = JSON.stringify({heading: 'test'})
+        ctx.state.page = await retriever.getPage('about')
+        console.log()
+        await ctx.render(ctx.state.page.meta.template, ctx.state.page)
+    },
+    editor: async(ctx) => {
+        ctx.body = '<h1>editor</h1>'
+    },
+}
+
+// Page routes
+app.use(router.get('/wf-editor', pages.editor))
+
+app.use(router.get('/', pages.home))
+app.use(router.get('/about', pages.about))
 
 app.use(async(ctx, next) => {
 	ctx.state.page = await retriever.getPage('home')
