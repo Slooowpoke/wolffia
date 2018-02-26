638b4adfa82ce52d999847266a50484f7cd3f16f
Mali Coxon <malii.coxon@gmail.com>
2018-02-26 22:01:26 +0000

    Added api routes

--- core/index.js
+++ core/index.js
@@ -38,9 +38,74 @@ app.use(async(ctx, next) => {
 	ctx.state.page = await retriever.getPage('home')
 	await next()
 })
+// TODO Authentication on API Routes
+const api = {
+	pages: async(ctx) => {
+        ctx.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+        let pages = await retriever.getAllPageMetas()
+    	ctx.body = JSON.stringify(pages)
+	},
+    blocks: async(ctx, pageID) => {
+        ctx.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+        let blocks = await retriever.getBlocksForPage(pageID)
+        ctx.body = JSON.stringify(blocks)
+    },
 
-app.use(async function(ctx, next) {
-	await ctx.render(ctx.state.page.meta.template, ctx.state.page)
-})
+    savePageMeta: async(ctx, pageID) => {
+        ctx.accepts('application/json')
+        let requestBody = ctx.request.body
+        ctx.body = await JSON.stringify(retriever.savePageMeta(requestBody.title, requestBody.name,requestBody.template, requestBody.id))
+    },
 
-app.listen(3000)
+    createPage: async(ctx) => {
+        ctx.accepts('application/json')
+        let requestBody = ctx.request.body
+        console.log(requestBody)
+
+        ctx.body = JSON.stringify(await retriever.createPage(requestBody.title, requestBody.name,requestBody.template))
+    },
+
+    blocksList: async(ctx) => {
+        ctx.body = JSON.stringify(await retriever.getBlocksList())
+    },
+
+    getBlockStructure: async(ctx, id) => {
+        let requestBody = ctx.request.body
+        ctx.body = JSON.stringify(await retriever.getBlockStructure(id))
+    },
+
+    createBlock: async(ctx) => {
+        ctx.accepts('application/json')
+        let requestBody = ctx.request.body
+        ctx.body = JSON.stringify(await retriever.createBlock(requestBody))
+    },
+
+    updatePageData: async(ctx, id) => {
+        ctx.accepts('application/json')
+        let requestBody = ctx.request.body
+        console.log(requestBody)
+        ctx.body = JSON.stringify(await retriever.updatePageData(requestBody, id))
+    },
+
+    deletePageData: async(ctx, pageID, blockID) => {
+        ctx.accepts('application/json')
+        let requestBody = ctx.request.body
+        ctx.body = JSON.stringify(await retriever.dropPageData(blockID))
+    }
+}
+
+
+// API Routes
+app.use(router.get('/api/pages', api.pages))
+app.use(router.get('/api/pages/:pageID/blocks', api.blocks))
+app.use(router.get('/api/blocks', api.blocksList))
+app.use(router.get('/api/blocks/:id', api.getBlockStructure))
+
+app.use(router.post('/api/page/:pageID/blocks/:blockID/delete', api.deletePageData))
+app.use(router.post('/api/page/:pageID/blocks', api.updatePageData))
+app.use(router.post('/api/pages/:pageID/meta/save', api.savePageMeta))
+app.use(router.post('/api/pages/create', api.createPage))
+app.use(router.post('/api/blocks/create', api.createBlock))
+
+
+app.listen(3001)
