b577a55688c1e072392c29265aa987d302ed3bdb
Mali Coxon <malii.coxon@gmail.com>
2018-02-26 21:58:56 +0000

    Formatting

--- core/fetch/Connection.js
+++ core/fetch/Connection.js
@@ -131,12 +131,14 @@ export default class Connection {
     async queryForPage(pagename){
         try {
             const meta = await this.queryForPageMeta(pagename)
+
 			const [blocks] = await this.db.execute('SELECT `data`,`blocks`.`template` FROM `page_data` LEFT JOIN `blocks` ON `page_data`.`block`=`blocks`.`id` WHERE `page_data`.`page`= ?', [meta.id])
 
             for(let block of blocks){
                 // TODO Find a non-blocking method
                 block.data = JSON.parse(block.data)
             }
+
 			return {meta, blocks}
 		} catch (error) {
 			console.log(error)
@@ -147,7 +149,7 @@ export default class Connection {
 	async queryForPageMeta(pagename) {
 		try {
 			const [results, fields] = await this.db.execute('SELECT * FROM `pages` WHERE `name` = ? LIMIT 1', [pagename])
-            
+
 			return results[0]
 		} catch (error) {
 			console.log(error)
