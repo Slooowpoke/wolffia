70821d194da51fbc52cae0e1cd335e74d5fde6c3
Mali Coxon <malii.coxon@gmail.com>
2018-03-04 22:55:16 +0000

    Fixed up FieldRepeating and added ui to create more rows

--- core/editor/src/components/fields/FieldRepeating.js
+++ core/editor/src/components/fields/FieldRepeating.js
@@ -18,25 +18,61 @@ class FieldRepeating extends Field {
 		}
 	}
 
-	update = (e) => {
+	update = (value, updateField, key, index) => {
+        console.log('update block')
+        console.log(updateField)
+        console.log(value)
+        console.log(key)
+
 		let field = this.state.field
-		field.value = e.target.value
+
+        field.value[index][key].value = value
 		this.setState({field: field})
-		this.props.update(e, field, this.state.key)
+		this.props.update(field.value, field, this.state.key, this.props.index)
 	}
+
+    getDescendantProp(obj, desc) {
+        var arr = desc.split('.')
+        while (arr.length && (obj = obj[arr.shift()]))
+            ;
+        return obj
+    }
 
 	render() {
-		return (<div>
-			{
-				this.props.field.value.map((object, index) => {
-					return Object.keys(object).map((key) => {
-						return FieldFactory.create(this.state.key + '.value' + '[' + index + ']' + '.' +  key, object[key], this.props.update)
-					})
-				})
-			}
-		</div>)
+		if(this.props.field.value.length === 0){
+			return (
+				<div>
+					<p>No rows added yet, trying adding some.</p>
+                    <button onClick={this.addRow}>Add row</button>
+                    <br /><br />
+				</div>
+
+			)
+		}else{
+            return (
+            	<div key={this.state.key}>
+                    <button onClick={this.addRow}>Add row</button>
+                    <br /><br />
+                {
+                    this.state.field.value.map((object, index) => {
+                        return Object.keys(object).map((key) => {
+                            return FieldFactory.create(key, object[key], this.update, index)
+                        })
+                    })
+                }
+            	</div>
+			)
+		}
 	}
 
+	addRow = () =>{
+        let field = this.state.field
+		console.log(field)
+        field.value.push({...field.objectToCreate})
+        console.log(field)
+        this.setState({field: field})
+        this.props.update(field.value, field, this.state.key, this.props.index)
+    }
 }
 
 export default connect()(FieldRepeating)
