eecc28a5ea20927ea1f5408ccff93556f41e3691
Mali Coxon <malii.coxon@gmail.com>
2018-03-04 22:55:48 +0000

    Added file reducers to reducer composition

--- core/editor/src/components/reducers.js
+++ core/editor/src/components/reducers.js
@@ -3,4 +3,5 @@ import {projects} from '../reducers/projects'
 import {authentication} from '../reducers/login'
 import {pages} from '../reducers/pages'
 import {blocks} from '../reducers/blocks'
-export const app = combineReducers({projects, authentication, pages, blocks})
+import {files} from '../reducers/files'
+export const app = combineReducers({projects, authentication, pages, blocks, files})
