bd63b04b21fb0a36f9fd5a8831f76dff6b89e1fb
Mali Coxon <malii.coxon@gmail.com>
2018-03-06 22:58:27 +0000

    Cleared out App.js

--- core/editor/src/App.js
+++ core/editor/src/App.js
@@ -6,17 +6,6 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo"/>
-					<h1 className="App-title">HAIII TOMEK</h1>
-				</header>
-                
-				<p className="App-intro">
-					I AM GHOST OF CHRIMBO PAST
-                    <br/>
-                    <code>src/App.js</code>
-					spoooky
-				</p>
 			</div>
 		);
 	}
