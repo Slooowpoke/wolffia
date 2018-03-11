1f4d5091130bec5e32cdd639ccaef6db9191c8db
Mali Coxon <malii.coxon@gmail.com>
2018-03-11 22:14:38 +0000

    Added styles to PageEditor

--- core/editor/src/components/screens/PageEditor.js
+++ core/editor/src/components/screens/PageEditor.js
@@ -5,6 +5,7 @@ import * as Actions from '../../actions/pages'
 
 import {Link, Route} from 'react-router-dom'
 import VisibleBlocks from '../blocks/VisibleBlocks'
+import {push} from 'react-router-redux'
 
 class PageEditor extends Component {
 	constructor(props) {
@@ -39,17 +40,73 @@ class PageEditor extends Component {
 
 	render() {
 		return (
-			<div>
-				<p>Viewing page: {this.props.match.params.id}</p>
-                <input type="text" name="title" value={this.state.title} onChange={this.onChangeTitle}/>
-                <input type="text" name="name" value={this.state.name} onChange={this.onChangeName}/>
-                <input type="text" name="name" value={this.state.template} onChange={this.onChangeTemplate}/>
-                <button onClick={this.save}>Save page meta</button>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h1>Editing page: {this.state.name}</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <button className="btn btn-outline btn-full" onClick={(e) => this.navigate(e, '/')}>Pages</button>
+                    </div>
+                    <div className="col">
+                        <button className="btn btn-outline btn-full" onClick={(e) => this.navigate(e, '/static')}>Static Content</button>
+                    </div>
+                    <div className="col">
+                        <button className="btn btn-outline btn-full" onClick={(e) => this.navigate(e, '/settings')}>Settings</button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <div className="block-outline">
+                            <span className="box-label">Page Meta</span>
+                            <div className="row">
+                                <div className="col col-sm-4">
+                                    <label>
+                                        <span>
+                                        Page Title:
+                                        </span>
+                                        <input type="text" name="title" value={this.state.title} onChange={this.onChangeTitle}/>
+                                    </label>
+                                </div>
+                                <div className="col col-sm-4">
+                                    <label>
+                            <span>
+                                Page Name:
+                            </span>
+                                        <input type="text" name="name" value={this.state.name} onChange={this.onChangeName}/>
+                                    </label>
+                                </div>
+                                <div className="col col-sm-4">
+                                    <label>
+                            <span>
+                              Page Template:
+                            </span>
+                                        <input type="text" name="name" value={this.state.template} onChange={this.onChangeTemplate}/>
+                                    </label>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col">
+                                    <button className="btn btn-outline align-right margin-top" onClick={this.save}>Save page</button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
 
                 <VisibleBlocks pageID= {this.state.id}/>
+
 			</div>
 		)
 	}
+
+    navigate= (e,page) => {
+        const { dispatch } = this.props
+        dispatch(push(page))
+    }
 
     onChangeTemplate = (e) =>{
         this.setState({template: e.target.value})
@@ -66,6 +123,7 @@ class PageEditor extends Component {
     save = () => {
         const { dispatch } = this.props
         let meta = {title: this.state.title, name: this.state.name,template: this.state.template, id: this.state.id}
+
         dispatch(Actions.updatePage(meta))
     }
 
