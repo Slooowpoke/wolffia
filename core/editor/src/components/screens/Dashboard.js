b73af4e2acaaa239c2248af488839f02a9773941
Mali Coxon <malii.coxon@gmail.com>
2018-03-11 22:14:56 +0000

    Added styles and navigation to Dashboard screen

--- core/editor/src/components/screens/Dashboard.js
+++ core/editor/src/components/screens/Dashboard.js
@@ -3,6 +3,7 @@ import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 
 import VisiblePages from '../pages/VisiblePages'
+import {push} from 'react-router-redux'
 
 class Dashboard extends Component {
 
@@ -10,14 +11,31 @@ class Dashboard extends Component {
 		super(props)
 	}
 
+    navigate= (e, page) => {
+        const { dispatch } = this.props
+        dispatch(push(page))
+    }
+
 	render() {
-		return (
+		return(
 			<div className="container">
 				<div className="row">
-					<div className="col col-sm-6">
-                        <p>Welcome blah, make something special.</p>
+                    <div className="col">
+                        <h1>Welcome Name.</h1>
+						<p className="small">Go make something special.</p>
 					</div>
 				</div>
+                <div className="row">
+                    <div className="col">
+                        <button className="btn btn-outline btn-full" onClick={(e) => this.navigate(e, '/')}>Pages</button>
+                    </div>
+                    <div className="col">
+                        <button className="btn btn-outline btn-full" onClick={(e) => this.navigate(e, '/static')}>Static Content</button>
+                    </div>
+                    <div className="col">
+                        <button className="btn btn-outline btn-full" onClick={(e) => this.navigate(e, '/settings')}>Settings</button>
+                    </div>
+                </div>
                 <VisiblePages />
 			</div>
 		)
