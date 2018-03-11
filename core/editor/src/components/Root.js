9177fd0cc509ba3e4fa54aff7760eb10685bbde6
Mali Coxon <malii.coxon@gmail.com>
2018-03-11 22:17:52 +0000

    Added settings route to editor

--- core/editor/src/components/Root.js
+++ core/editor/src/components/Root.js
@@ -9,6 +9,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Landing from './screens/Landing'
 import Dashboard from './screens/Dashboard'
 import PageEditor from './screens/PageEditor'
+import Settings from './screens/Settings'
 
 import {ConnectedRouter} from 'react-router-redux'
 import AuthenticatedComponent from './signin/AuthenticatedComponent'
@@ -20,6 +21,7 @@ const Root = ({store, history}) => (
 				<Route exact path="/landing" component={Landing}/>
                 <Route exact path="/" component={AuthenticatedComponent(Dashboard)}/>
                 <Route exact path="/pages/:id" component={AuthenticatedComponent(PageEditor)}/>
+                <Route exact path="/settings" component={AuthenticatedComponent(Settings)}/>
 			</div>
 		</ConnectedRouter>
 	</Provider>
