a4fac7da9d7915a1a82224eb0b62e8b3fba3b85a
Mali Coxon <malii.coxon@gmail.com>
2018-03-13 22:14:29 +0000

    Added method for getting block html by name

--- core/models/Page.js
+++ core/models/Page.js
@@ -6,4 +6,13 @@ export default class Page {
         this.blocks = blocks
     }
 
+    getBlock(name){
+        for(let block of this.blocks){
+            // console.log(block)
+            if(block.name === name){
+                return block.html
+            }
+        }
+        return 'Block not found'
+    }
 }
