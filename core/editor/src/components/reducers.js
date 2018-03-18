463e2382f8bd0232b9286e1c140003342e30951e
Mali Coxon <malii.coxon@gmail.com>
2018-03-18 18:42:33 +0000

    Added structure to combineReducers

--- core/editor/src/components/reducers.js
+++ core/editor/src/components/reducers.js
@@ -4,4 +4,5 @@ import {authentication} from '../reducers/login'
 import {pages} from '../reducers/pages'
 import {blocks} from '../reducers/blocks'
 import {files} from '../reducers/files'
-export const app = combineReducers({projects, authentication, pages, blocks, files})
+import {structure} from '../reducers/structure'
+export const app = combineReducers({projects, authentication, pages, blocks, files, structure})
