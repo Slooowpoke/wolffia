564620760117cc598a222d69eb67e9a8784e7af5
Mali Coxon <malii.coxon@gmail.com>
2018-03-19 22:26:17 +0000

    Simplified and refactored reducers
    Changed Object.assign to Spread operator

--- core/editor/src/reducers/blocks.js
+++ core/editor/src/reducers/blocks.js
@@ -1,11 +1,7 @@
 import {
-    LOAD_BLOCKS_REQUEST,
-    LOAD_BLOCKS_FAILURE,
-    LOAD_BLOCKS_SUCCESS,
     CREATE_BLOCK,
     UPDATE_BLOCK,
     DELETE_BLOCK_SUCCESS,
-    CREATE_BLOCK_EDITOR,
     CLEAR_CURRENT_EDITOR
 } from '../actions/blocks'
 
@@ -18,19 +14,20 @@ const initialState = {
 
 export function blocks(state = initialState, action) {
 	switch (action.type) {
-        case CREATE_BLOCK_EDITOR:
-            return Object.assign({}, state, {
-                currentEditorBlock: action.currentEditorBlock
-            })
-		case CREATE_BLOCK:
-			return Object.assign({}, state, {
-				list: [
-					...state.list, action.block
-				]
-			})
+        case 'LOAD_BLOCK_EDITOR_STRUCTURE_SUCCESS':
+            return {
+                ...state,
+                currentEditorBlock: action.response
+            }
+        case CREATE_BLOCK:
+		    return {
+                ...state,
+                list: [
+                    ...state.list, action.block
+                ]
+            }
         case UPDATE_BLOCK:
-            if(action.blockID == undefined){
-
+            if(action.blockID === undefined){
                 return {
                     ...state,
                     list:[
@@ -41,41 +38,42 @@ export function blocks(state = initialState, action) {
                     ]
                 }
             }else{
-                return Object.assign({}, state, {
+                return {
+                    ...state,
                     list: state.list.map(block => {
                         if(block.id == action.blockID){
                             block = action.block
                         }
                         return block
                     })
-                })
+                }
             }
-		case DELETE_BLOCK_SUCCESS:
-			return Object.assign({}, state, {
-				list: state.list.filter(block => block.id != action.remove)
-			})
-		case LOAD_BLOCKS_REQUEST:
-			return Object.assign({}, state, {
-				isFetching: true,
-				didInvalidate: false
-			})
-		case LOAD_BLOCKS_SUCCESS:
+        case DELETE_BLOCK_SUCCESS:
+		    return {
+                ...state,
+               list: state.list.filter(block => block.id !== action.remove)
+            }
+        case 'LOAD_BLOCKS_REQUEST':
+		    return {
+                ...state,
+                isFetching: true
+            }
+		case 'LOAD_BLOCKS_SUCCESS':
             return {
                 ...state,
                 isFetching: false,
-				didInvalidate: false,
 				list: [
-					...action.list
+					...action.response
 				],
 				lastUpdated: action.receivedAt
             }
-		case LOAD_BLOCKS_FAILURE:
-			return Object.assign({}, state, {
-				isFetching: false,
-				didInvalidate: false,
+        case 'LOAD_BLOCKS_FAILURE':
+		    return {
+                ...state,
+                isFetching: false,
                 error: action.error,
-				lastUpdated: action.receivedAt
-			})
+                lastUpdated: action.receivedAt
+            }
         case CLEAR_CURRENT_EDITOR:
             return {
                 ...state,
