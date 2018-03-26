b3a0e2017ede9563347d2ebbef6b52ce27d393b8
Mali Coxon <malii.coxon@gmail.com>
2018-03-26 22:45:18 +0100

    Removed dead code

--- core/editor/src/components/reducers.js
+++ core/editor/src/components/reducers.js
@@ -1,8 +1,7 @@
 import {combineReducers} from 'redux'
-import {projects} from '../reducers/projects'
 import {authentication} from '../reducers/login'
 import {pages} from '../reducers/pages'
 import {blocks} from '../reducers/blocks'
 import {files} from '../reducers/files'
 import {structure} from '../reducers/structure'
-export const app = combineReducers({projects, authentication, pages, blocks, files, structure})
+export const app = combineReducers({ authentication, pages, blocks, files, structure})
