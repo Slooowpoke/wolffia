8faaba9a582fd3e5f5531b14ce7bdbd68019987c
Mali Coxon <malii.coxon@gmail.com>
2018-03-26 13:13:48 +0100

    Fixed id issues

--- core/editor/src/components/screens/PageEditor.js
+++ core/editor/src/components/screens/PageEditor.js
@@ -1,17 +1,18 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 
 import VisibleBlocksContainer from '../containers/VisibleBlocksContainer'
 import Header from '../Header'
-import Navigation from '../Navigation'
 
 class PageEditor extends Component {
-	constructor(props) {
-		super(props)
-	}
+    constructor(props) {
+        super(props)
 
-	render() {
-	    let title = 'Editing page:' + this.props.name;
-		return (
+
+    }
+
+    render() {
+        let title = 'Editing page:' + this.props.name
+        return (
             <div className="container-fluid">
                 <Header title={title}/>
                 <div className="row">
@@ -24,29 +25,34 @@ class PageEditor extends Component {
                                         <span>
                                         Page Title:
                                         </span>
-                                        <input type="text" name="title" value={this.props.title} onChange={this.props.onChangeTitle}/>
+                                        <input type="text" name="title" value={this.props.title}
+                                               onChange={this.props.onChangeTitle}/>
                                     </label>
                                 </div>
                                 <div className="col col-sm-4">
                                     <label>
-                            <span>
-                                Page Name:
-                            </span>
-                                        <input type="text" name="name" value={this.props.name} onChange={this.props.onChangeName}/>
+                                        <span>
+                                            Page Name:
+                                        </span>
+                                        <input type="text" name="name" value={this.props.name}
+                                               onChange={this.props.onChangeName}/>
                                     </label>
                                 </div>
                                 <div className="col col-sm-4">
                                     <label>
-                            <span>
-                              Page Template:
-                            </span>
-                                        <input type="text" name="name" value={this.props.template} onChange={this.props.onChangeTemplate}/>
+                                        <span>
+                                          Page Template:
+                                        </span>
+                                        <input type="text" name="name" value={this.props.template}
+                                               onChange={this.props.onChangeTemplate}/>
                                     </label>
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="col">
-                                    <button className="btn btn-outline align-right margin-top" onClick={this.props.save}>Save page</button>
+                                    <button className="btn btn-outline align-right margin-top"
+                                            onClick={this.props.save}>Save page
+                                    </button>
                                 </div>
                             </div>
 
@@ -54,10 +60,10 @@ class PageEditor extends Component {
                     </div>
                 </div>
 
-                <VisibleBlocksContainer pageID= {this.props.id} />
-			</div>
-		)
-	}
+                <VisibleBlocksContainer pageID={this.props.id}/>
+            </div>
+        )
+    }
 
 }
 
