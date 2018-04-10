08e3d6f191666148e0a4bc8a98aa841393fb0788
Mali Coxon <malii.coxon@gmail.com>
2018-04-10 22:58:19 +0100

    Added header classes to header

--- core/editor/src/components/Header.js
+++ core/editor/src/components/Header.js
@@ -8,7 +8,7 @@ class Header extends Component {
 
     render() {
         return (
-            <h1>{this.props.title}</h1>
+            <h1 className="h1">{this.props.title}</h1>
         )
     }
 
